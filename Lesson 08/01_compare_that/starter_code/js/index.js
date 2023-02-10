2afff4e77d44d2593f65d0e2f84a7675bff98d94
Melimari <melinath5@hotmail.com>
2023-02-09 19:48:42 -0600

    new changes

---
+++ Lesson 08/01_compare_that/starter_code/js/index.js
@@ -0,0 +1,40 @@
+
+ document.querySelector('#submit').onclick = function(e) {
+    e.preventDefault();
+     let comparison;
+     let a = Number(document.querySelector('#a').value);
+     let b = Number(document.querySelector('#b').value);
+ 
+     // Add comparison logic
+ 
+// if a is greater than b
+
+    if (a > b) {
+      comparison = ">";
+    }
+
+    // if b is greater than a
+    if (b > a) {
+      comparison = "<";
+    }
+
+    // if they are equal
+    if (a == b) {
+      comparison = "==";
+    }
+
+    // if a OR b is not a number
+    if (isNaN(a) || isNaN(b)) {
+      comparison = "N/A";
+    }
+    
+
+    
+
+
+
+
+
+     document.querySelector('#comparison').innerText = comparison;
+   };
+ 
