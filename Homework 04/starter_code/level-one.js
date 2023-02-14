59971034493daa4fb83d705b608d52d2d5319524
Melimari <melinath5@hotmail.com>
2023-02-14 00:21:58 -0600

    new js

--- Homework 04/starter_code/level-one.js
+++ Homework 04/starter_code/level-one.js
@@ -1,11 +1,30 @@
-/*
- * DOTS: Level One
- *
- */
+//  DOTS: Level One
+
 let score = 0;
 let ballEl = document.querySelector('.js-ball');
 let scoreEl = document.querySelector('.js-score');
 
+
+//  When ball is clicked, a score of 10 is added
+ballEl.addEventListener('click', 
+function(){
+score += 10;
+});
+
+
+// When the score reaches 100, the game declares a winner
+if ( score < 100 ) {
+  scoreEl.innerText = score;
+} else {
+  declareWinner();
+};
+
+// The screen asks the user to click to proceed to next level
+
 function declareWinner() {
   document.body.classList.add('game-over');
 }
+
+
+
+
--- Homework 04/starter_code/level-two.js
+++ Homework 04/starter_code/level-two.js
@@ -7,3 +7,20 @@ let arenaEl = document.querySelector('.js-arena');
 let scoreEl = document.querySelector('.js-score');
 
 
+// when any white ball is clicked, a score of 10 is added
+
+arenaEl.addEventListener('click',
+function(){
+score += 10;
+});
+
+if (score < 100) {
+    scoreEl.innerText = score;
+} 
+else {
+    declareWinner();
+  };
+
+  function declareWinner() {
+    document.body.classList.add('game-over');
+  }
