d0c4c855613e6652db2edb8773ccaee3e72fdb32
Melimari <melinath5@hotmail.com>
2023-02-27 00:46:12 -0600

    final project 2 edits

---
+++ Final Project 2/main.js
@@ -0,0 +1,20 @@
+// variables needed for age 55 and up, military, and airline crew
+
+const age = >==55
+const airline = true
+const military = true
+
+// if all 3 are true, display 30% discount box
+function{
+    if age = >= 55
+    (console.log)
+}
+
+else {
+    
+}
+
+// if only one is true, display 30% discount
+
+
+// if none are true, display no discount
