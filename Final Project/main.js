c4e4d77802deb71ecd76342ee03bd72b4adad303
Melimari <melinath5@hotmail.com>
2023-03-16 00:33:02 -0500

    final project edits

--- Final Project/main.js
+++ Final Project/main.js
@@ -1,56 +1,50 @@
 //Discount form area
 
- function checkButton() {  
-      if(document.getElementById('yes').checked) { 
-          document.getElementById("disp").innerHTML 
-             = 
-            " You qualify for a discount! Contact us with your airline crew employee number"; 
-      } 
+function checkButton() {  
+    let responses = []
+    
+    if(document.querySelector('input[name=airline]:checked') != null){
+    
+    
+        let airlineValue = document.querySelector('input[name=airline]:checked').value;
+        // console.log(airlineValue)
+        if(airlineValue.toLowerCase() == "yes") { 
+         
+             responses.push("You qualify for a discount! Contact us with your airline crew employee number"); 
+            } 
+    
+        }
+        if(document.querySelector('input[name=military]:checked') != null){
+            if(document.querySelector('input[name=military]:checked').value.toLowerCase() == "yes") { 
+        
+                responses.push("You qualify for a discount! Contact us with your military credentials"); 
+            } 
+        }
+     
+        if(document.querySelector('input[name=senior]:checked') != null){
+       if(document.querySelector('input[name=senior]:checked').value.toLowerCase() == "yes") { 
+         
+        responses.push("You qualify for a discount! Email us with a copy of your license or I.D"); 
+            } 
+        }
+       
+    
+       if(responses.length == 0) { 
+        responses.push(" Sorry, no discount available at this time");   
+       }
+    
+       let display = ""
+       for(i=0; i < responses.length; i++){
+        display = display + "<li>"+responses[i]+"</li>"
+       }
+    
+       document.getElementById("disp").innerHTML = display
+    } 
 
-     if(document.getElementById('yes2').checked) { 
-         document.getElementById("disp").innerHTML 
-             = 
-              " You qualify for a discount! Contact us with your military credentials"; 
-     } 
-
-     if(document.getElementById('yes3').checked) { 
-         document.getElementById("disp").innerHTML 
-             =               " You qualify for a discount! Email us with a copy of your license or I.D"; 
-     } 
-
-     else if(document.getElementById('no').checked) { 
-         document.getElementById("disp").innerHTML 
-            = 
-             " Sorry, no discount available at this time";   
-     }
- } 
-
-// Newletter form area 
+// Newletter Form Area 
 
  function submitForm() {
     let userFirstName = document.getElementById('fname').value;
     let userLastName = document.getElementById('lname').value;
     alert("Thank you" + " " + userFirstName + "! We added you to our mailing list!")
 }
-
-
-
-  // function checkButton() {
-  //   let discountApprove = document.getElementById('yes').checked;
-  //   let discountDeny = document.getElementById('no').checked;
-
-
-  //   if(discountApprove == 'yes'){
-  //       document.getElementById("disp").innerHTML 
-  //       ="discount";
-
-  //   }
-
-  //   if(discountApprove == "no"){
-  //       document.getElementById("disp").innerHTML 
-  //       ="sorry";
-
-  //   }
-  // }
-   
-
